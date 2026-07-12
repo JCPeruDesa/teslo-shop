b15ed769e267df90a2dae54d7f783d2a5cf3afcf
Jorge Clemente <jclementeh@gmail.com>
2026-07-12 17:09:19 -0500

    mostrar detalle del producto

--- src/app/store-front/pages/product-page/product-page.ts
+++ src/app/store-front/pages/product-page/product-page.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-product-page',
   imports: [],
   templateUrl: './product-page.html',
 })
-export class ProductPage {}
+export class ProductPage {
+  activatedRoute = inject(ActivatedRoute);
+  productService = inject(ProductsService);
+
+  productIdSlug = toSignal(
+    this.activatedRoute.params.pipe(
+      map(params => params['idSlug'])
+    )
+  );
+
+  productResource = rxResource({
+    params: () => ({ idSlug: this.productIdSlug() }),
+    stream: ({ params }) => this.productService.getProductBIdSlug(params.idSlug),
+  });
+
+}
