b04d1957c922ac2f77db8cb393d079c23bfb2925
Jorge Clemente <jclementeh@gmail.com>
2026-09-13 18:45:07 -0500

    Implementa servicio de paginacion

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -4,24 +4,30 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductCard } from '@products/components/product-card/product-card';
 import { ProductsService } from '@products/services/products.service';
 import { map } from 'rxjs/operators';
+import { Pagination } from '@shared/components/pagination/pagination';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCard],
+  imports: [ProductCard, Pagination],
   templateUrl: './gender-page.html',
 })
 export class GenderPage {
   route = inject(ActivatedRoute);
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
   gender = toSignal(this.route.params.pipe(
     map(({ gender }) => gender)));
 
     productsResource = rxResource({
-    params: () => ({gender: this.gender()}),
+    params: () => ({gender: this.gender(),
+                    page: this.paginationService.currentPage(),
+                    offset: (this.paginationService.currentPage() - 1) * 9}),
     stream: ({ params }) => {
       return this.productsService.getProducts(params);
     },
   });
 }
+
--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,21 +1,29 @@
 import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+
 import { ProductCard } from '../../../products/components/product-card/product-card';
 import { ProductsService } from '../../../products/services/products.service';
-import { rxResource } from '@angular/core/rxjs-interop';
+
+import { Pagination } from '@shared/components/pagination/pagination';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCard],
+  imports: [ProductCard, Pagination],
   templateUrl: './home-page.html',
 })
 export class HomePage {
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
   productsResource = rxResource({
-    params: () => ({}),
+    params: () => ({page: this.paginationService.currentPage()}),
     stream: ({ params }) => {
-      return this.productsService.getProducts({});
+      return this.productsService.getProducts({
+        offset: (params.page - 1) * 10,
+      });
     },
   });
 }
+
